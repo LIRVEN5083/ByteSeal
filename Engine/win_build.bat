ad66fd7990f325c4296076be70a28d0857e608b8
Vincent <lirvenxd@gmail.com>
2026-06-01 14:04:00 +0300

    Change a litle auto build

--- Engine/win_build.bat
+++ Engine/win_build.bat
@@ -1,2 +1,10 @@
 @echo off
+
+if exist "out" ( rmdir /s /q "out" )
+
 cmake -B out/build/Debug -G Ninja -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang && cmake --build out/build/Debug
+
+if %errorlevel% neq 0 (
+    pause
+)
+	
--- Engine/win_build_test.bat
+++ Engine/win_build_test.bat
@@ -1,4 +1,7 @@
 @echo off
+
+if exist "out" ( rmdir /s /q "out" )
+
 cmake -B out/build/Debug -G Ninja -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
 
 if %errorlevel% equ 0 (
@@ -7,4 +10,7 @@ if %errorlevel% equ 0 (
 
 if %errorlevel% equ 0 (
     start cmd /K "cd /d "%~dp0out\build\Debug" && ByteSeal.exe"
+)
+else(
+    pause
 )
