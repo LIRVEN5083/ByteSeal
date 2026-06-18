cc1f76ccb06be47b5704ec9e0ca2e69479eb0457
Vincent <andr.sin81@gmail.com>
2026-06-18 17:16:04 +0300

    Fix win_build_test.bat

--- Vulkan-guide/win_build_test.bat
+++ Vulkan-guide/win_build_test.bat
@@ -4,13 +4,8 @@ if exist "out" ( rmdir /s /q "out" )
 
 cmake -B out/build/Debug -G Ninja -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
 
-if %errorlevel% equ 0 (
-    cmake --build out/build/Debug
-)
+cmake --build out/build/Debug
 
-if %errorlevel% equ 0 (
-    start cmd /K "cd /d "%~dp0out\build\Debug" && ByteSeal.exe"
-)
-else(
-    pause
-)
+start cmd /K "cd /d "%~dp0out\build\Debug" && ByteSeal.exe"
+    
+pause
