eb97d619a4385b22a65b72ab09677ca712af2489
Victor Ruta <andr.sin81@gmail.com>
2026-07-18 10:50:30 +0300

    fix speed

--- Engine_try1/src/Controller/movement.cpp
+++ Engine_try1/src/Controller/movement.cpp
@@ -1,11 +1,11 @@
 #include "movement.hpp"
 
 void CONTROLLER::IncreaseSpeed(float& speed) {
-    speed += 0.1;
+    speed += 0.06;
 }
 
 void CONTROLLER::DecreaseSpeed(float& speed) {
     if (speed > 0.2) {
-        speed -= 0.1;
+        speed -= 0.06;
     }
 }
