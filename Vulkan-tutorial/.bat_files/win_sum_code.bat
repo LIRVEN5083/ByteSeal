d2325eec9916f0ec4d7c06914e145dad075abde7
Victor Ruta <andr.sin81@gmail.com>
2026-08-30 22:18:56 +0300

    Fix project directories

--- Vulkan-tutorial/.bat_files/win_sum_code.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Code stroke:
-
-powershell -Command "((& '.\cloc-2.08.exe' --exclude-dir=external '%~dp0..\src' | Select-String 'SUM:') -split '\s+' | Where-Object { $_ -match '^\d+$' } | Measure-Object -Sum).Sum"
-\
-
-
-pause
